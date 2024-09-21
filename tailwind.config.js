90bff64a28d68a437359d2bbff7a9147787310f0
MozahidNahin <mozahidulislamnahin@gmail.com>
2024-09-21 17:28:29 +0600

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        // color: {
+        //     'button-green': 'rgba(29, 209, 0, 0.40)'
+        // }
+      },
+    },
+    plugins: [],
+  }
